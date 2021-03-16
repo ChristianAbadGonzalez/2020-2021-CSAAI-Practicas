1dac6b40b0e288aff891ac32a94f817f259f885e
ChristianAbadGonzalez <christianmillan179@gmail.com>
2021-03-16 13:03:02 +0100

    Introducción al lenguaje JavaScript -- Ejercicio 3

---
+++ P2/S4/Ejemplo_3.js
@@ -0,0 +1,14 @@
+// Esto es la manera en el lenguaje de programación de JavaScript de realizar un comentario...
+
+// Exite otra manera de poner comentarios en JavaScript,
+// que es como lo haciamos en los ficheros de CSS, es decir, /* */
+
+/* Esto es la manera en el lenguaje de programación de JavaScript de realizar un comentario... */
+
+// Vamos a implementar la impresión de tres mensajes
+// en la consola de nuestro navegador.
+
+console.log("Por fin... Estoy fuera del lenguaje de HTML...");
+console.log("Se remienda hacerlo de esta manera...");
+console.log("Recomendable NO --> OBLIGATORIO");
+console.log("----------------------------------------------");
