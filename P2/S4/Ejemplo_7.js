c560e4d7f6624b8dd4584e3ef90916f17b365aba
ChristianAbadGonzalez <christianmillan179@gmail.com>
2021-03-16 18:19:32 +0100

    Introducción al lenguaje JavaScript -- Ejercicio 8

---
+++ P2/S4/Ejemplo_7.js
@@ -0,0 +1,9 @@
+console.log("Ejecutando JavaScript...")
+
+/* Leer el párrafo identificado como 'test' */
+const test = document.getElementById('test')
+
+/* Configurar el manejador para el evento de pulsación de botón */
+test.onclick = function (){
+    console.log("Click sobre el párrafo...")
+}
