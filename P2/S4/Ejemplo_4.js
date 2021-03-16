d093a4b205eaf60f67e217abb66d559bbd75ff39
ChristianAbadGonzalez <christianmillan179@gmail.com>
2021-03-16 13:11:06 +0100

    Introducción al lenguaje JavaScript -- Ejercicio 4

---
+++ P2/S4/Ejemplo_4.js
@@ -0,0 +1,3 @@
+console.log("La página de HTML se ha cargado por completo");
+console.log("Ahora se con seguridad que se puede ejecutar el codigo de JavaScript");
+console.log("Acción 'defer' Activada...")
