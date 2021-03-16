ffb29a9b1d7a5c980088924378cd0393cf49456a
ChristianAbadGonzalez <christianmillan179@gmail.com>
2021-03-16 17:24:31 +0100

    Introducción al lenguaje JavaScript -- Ejercicio 5

---
+++ P2/S4/Plantilla.js
@@ -0,0 +1,3 @@
+/* Punto d entrada: Una vez cargada la página de HTML, se procede a ejecutar el código de JavaScript */
+console.log("Aquí comienza tu código de JavaScript...");
+console.log("¡¡Que comiencen los juegos del JavaScript!!");
