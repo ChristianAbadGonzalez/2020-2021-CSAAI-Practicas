33a0c1dc798ee2aa0f2be08f93b7d5a269db98c0
ChristianAbadGonzalez <christianmillan179@gmail.com>
2021-04-08 09:31:53 +0200

    Ejemplos de la sesión S6 - Ejemplo 1

---
+++ P3/S6/Ejemplo_1.js
@@ -0,0 +1,8 @@
+console.log("Ejecutando JavaScript...");
+
+const canvas = document.getElementById("canvas");
+
+/* Definimos el tamaño que va a tener el canvas */
+
+canvas.width = 300;
+canvas.heigth = 100;
